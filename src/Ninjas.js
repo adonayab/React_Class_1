54838804852115b764c62fb0d068ee6c46b201cd
adonayab <adonayab15@hotmail.com>
2019-07-09 21:43:23 -0500

    form continued -- Removing items from the DOM

--- src/Ninjas.js
+++ src/Ninjas.js
@@ -1,45 +1,59 @@
 import React from "react";
 
-const Ninjas = ({ ninjas }) => {
-  // const ninjaList = ninjas.map(ninja => {
-  //   if (ninja.age > 20) {
-  //     return (
-  //       <div className="ninja" key={ninja.id}>
-  //         <div>Name: {ninja.name}</div>
-  //         <div>Age: {ninja.age}</div>
-  //         <div>Belt: {ninja.belt}</div>
-  //       </div>
-  //     );
-  //   } else {
-  //     return null;
-  //   }
-  // });
+const Ninjas = ({ ninjas, deleteNinja }) => {
+  const ninjaList = ninjas.map(ninja => {
+    if (ninja.age > 2) {
+      return (
+        <div className="ninja" key={ninja.id}>
+          <div>Name: {ninja.name}</div>
+          <div>Age: {ninja.age}</div>
+          <div>Belt: {ninja.belt}</div>
+          <button
+            onClick={() => {
+              deleteNinja(ninja.id);
+            }}
+          >
+            Delete ninja
+          </button>
+        </div>
+      );
+    } else {
+      return null;
+    }
+  });
 
   // const ninjaList = ninjas.map(ninja => {
-  //   return ninja.age > 20 ? (
+  //   return ninja.age > 2 ? (
   //     <div className="ninja" key={ninja.id}>
   //       <div>Name: {ninja.name}</div>
   //       <div>Age: {ninja.age}</div>
   //       <div>Belt: {ninja.belt}</div>
+  //       <button
+  //         onClick={() => {
+  //           deleteNinja(ninja.id);
+  //         }}
+  //       >
+  //         Delete ninja
+  //       </button>
   //     </div>
   //   ) : null;
   // });
 
-  // return <div className="ninja-list">{ninjaList}</div>;
+  return <div className="ninja-list">{ninjaList}</div>;
 
-  return (
-    <div className="ninja-list">
-      {ninjas.map(ninja => {
-        return ninja.age > 2 ? (
-          <div className="ninja" key={ninja.id}>
-            <div>Name: {ninja.name}</div>
-            <div>Age: {ninja.age}</div>
-            <div>Belt: {ninja.belt}</div>
-          </div>
-        ) : null;
-      })}
-    </div>
-  );
+  // return (
+  //   <div className="ninja-list">
+  //     {ninjas.map(ninja => {
+  //       return ninja.age > 2 ? (
+  //         <div className="ninja" key={ninja.id}>
+  //           <div>Name: {ninja.name}</div>
+  //           <div>Age: {ninja.age}</div>
+  //           <div>Belt: {ninja.belt}</div>
+  //         </div>
+  //       ) : null;
+  //     })}
+  //   </div>
+  // );
 };
 
 export default Ninjas;
