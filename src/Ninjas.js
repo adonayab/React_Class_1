8856ebc9a5f3aaa594c6e48c2f727b7e1d87f1ae
adonayab <adonayab15@hotmail.com>
2019-07-09 21:13:44 -0500

    Completed creation of form and adding contents from form to the DOM

--- src/Ninjas.js
+++ src/Ninjas.js
@@ -30,7 +30,7 @@ const Ninjas = ({ ninjas }) => {
   return (
     <div className="ninja-list">
       {ninjas.map(ninja => {
-        return ninja.age > 20 ? (
+        return ninja.age > 2 ? (
           <div className="ninja" key={ninja.id}>
             <div>Name: {ninja.name}</div>
             <div>Age: {ninja.age}</div>
